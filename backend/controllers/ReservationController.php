8925a20eef9e12a4a5deeea1da629db87a813f81
Gilvan-R-A <gilvan.dev@gmail.com>
2025-01-17 15:49:35 -0300

    feat: refactored ReservationController to be able to use model

--- backend/controllers/ReservationController.php
+++ backend/controllers/ReservationController.php
@@ -1,23 +1,30 @@
 <?php
 
-require_once '../config/database.php';
+require_once '../models/Reservation.php';
 
 class ReservationController {
+
     public static function index () {
-        global $pdo;
-        $stmt = $pdo->query('SELECT * FROM  reservations');
-        echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+        $reservation = new Reservation();
+        echo json_encode($reservation->getAllReservations());
     }
 
     public static function store() {
-        global $pdo;
-        $data = json_decode(file_get_contents('php://input'), true);
+       $data = json_decode(file_get_contents('php://input'), true);
+       $reservation = new Reservation();
 
-        $stmt = $pdo->prepare(
-            'INSERT INTO  reservations (room_number, start_time, end_time, customer_name, customer_email)
-             VALUES (:room_number, :start_time, :end_time, :customer_name, customer_email)'
-        );
-        $stmt->execute($data);
-        echo json_encode(['message' => 'Reserva criada com sucesso.']);
+       if ($reservation->checkConflict($data['start_time'], $data['end_time'], $data['room_number'] )){
+            http_response_code(409);
+            echo json_encode(['error' => 'Conflito de horário detectado.']);
+            return;
+       }
+
+       if ($reservation->createReservation($data)){
+            echo json_encode(['message' => 'Reserva criada com sucesso.']);
+       } else {
+        http_response_code(500);
+        echo json_encode(['error' => 'Erro ao criar reserva.']);
+       }
     }
 }
+
