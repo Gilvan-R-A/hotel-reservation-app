208388ae4a39968eaeab79c191fbe67f05ebbc6a
Gilvan-R-A <gilvan.dev@gmail.com>
2025-01-25 20:30:04 -0300

    fix: CORS settings implemented

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,7 +1,12 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 
 require_once '../routes/api.php';
