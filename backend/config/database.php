667fa8f41f1628eb8de56573dfd5185c2589af3c
Gilvan-R-A <gilvan.dev@gmail.com>
2025-04-19 15:29:04 -0300

    fix: configuration to make loading .env optional

--- backend/config/database.php
+++ backend/config/database.php
@@ -11,8 +11,12 @@ class Database
     public static function getConnection()
     {
         if (self::$pdo === null) {
-            $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-            $dotenv->load();
+            $dotenvPath = __DIR__ . '/../../';
+
+            if (file_exists($dotenvPath . '.env')) {
+                $dotenv = Dotenv::createImmutable($dotenvPath);
+                $dotenv->load();
+            }
 
             $host = $_ENV['DB_HOST'];
             $dbname = $_ENV['DB_NAME'];
@@ -30,109 +34,3 @@ class Database
         return self::$pdo;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <?php
-
-// require_once __DIR__ . '/../../vendor/autoload.php';
-
-// use Dotenv\Dotenv;
-
-// $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-// $dotenv->load();
-
-// $host = $_ENV['DB_HOST'];
-// $dbname = $_ENV['DB_NAME'];
-// $user = $_ENV['DB_USER'];
-// $password = $_ENV['DB_PASS'];
-// $port = $_ENV['DB_PORT'];
-
-// try {
-//     $pdo = new PDO("mysql:host=$host; port=$port; dbname=$dbname; charset=utf8mb4", $user, $password);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     die("Erro ao conectar ao banco de dados: " .$e->getMessage());
-// }
